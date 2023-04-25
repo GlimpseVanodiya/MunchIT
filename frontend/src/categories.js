e27e9f59a306cba8e58d2e67667ffc8c39996dd3
Zalak Vanodiya <91529386+GlimpseVanodiya@users.noreply.github.com>
2023-04-25 15:39:00 -0400

    Add files via upload

---
+++ frontend/src/categories.js
@@ -0,0 +1,17 @@
+const categories = [
+    {
+        name: "Vegetable",
+        img: "https://res.cloudinary.com/djtgxff6l/image/upload/v1676669943/Munchit/vegetables_a7cwmo.jpg",
+    },
+    {
+        name: "Fruit",
+        img: "https://res.cloudinary.com/djtgxff6l/image/upload/v1676669941/Munchit/fruits_d3voql.jpg",
+    },
+
+    { 
+        name: "Dairy, Eggs & Bread", 
+        img: "https://res.cloudinary.com/djtgxff6l/image/upload/v1676669936/Munchit/dairy_nafpar.jpg" 
+    }
+];
+
+export default categories;
